fdd0b7027b8ffde6b4c93fd6a14626ed033259d9
Davin <davin@concourselabs.com>
2021-01-24 15:33:41 -0800

    update to angular 11

--- src/router-state-params.service.ts
+++ src/router-state-params.service.ts
@@ -1,9 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Router, ActivatedRoute, NavigationEnd } from '@angular/router';
-
-import 'rxjs/Rx';
-import { Subject, BehaviorSubject } from 'rxjs';
-import { Observable } from 'rxjs/Observable';
+import { BehaviorSubject } from 'rxjs';
+import { map, filter } from 'rxjs/operators';
 
 /**
  * router state params service
@@ -40,118 +38,117 @@ export class RouterStateParamsService {
   params: Object;
   paramsSubject: BehaviorSubject<Object>;
 
-  constructor(
-      private activatedRoute: ActivatedRoute,
-      private router: Router
-  ) {
-
+  constructor(private activatedRoute: ActivatedRoute, private router: Router) {
     this.urlSubject = new BehaviorSubject<String>(null);
     this.pathSubject = new BehaviorSubject<String>(null);
     this.routeSubject = new BehaviorSubject<ActivatedRoute>(null);
     this.configSubject = new BehaviorSubject<Object>(null);
     this.paramsSubject = new BehaviorSubject(null);
 
-
     var allRoutes = [];
 
     this.router.events
-        .filter(event => event instanceof NavigationEnd)
-        .map(_ => this.router.routerState.root)
-        .map(route => {
+      .pipe(
+        filter((event) => event instanceof NavigationEnd),
+        map((_) => this.router.routerState.root),
+        map((route) => {
           allRoutes = [];
 
           allRoutes.push({
             config: route.routeConfig,
-            params: route.params
+            params: route.params,
           });
-          while (route.firstChild){
+          while (route.firstChild) {
             route = route.firstChild;
             allRoutes.push({
               config: route.routeConfig,
-              params: route.params
+              params: route.params,
             });
           }
           return allRoutes;
         })
-        .subscribe(data => {
-          this.route = activatedRoute;
-          this.url = router.url;
-          this.config = data[data.length-1].config;
+      )
+      .subscribe((data) => {
+        this.route = activatedRoute;
+        this.url = router.url;
+        this.config = data[data.length - 1].config;
 
-          let paths = [];
-          this.params = {};
-          if(allRoutes.length){
-            allRoutes.map( val => {
-              if(val && val.params && val.params.value){
-                this.params = Object.assign(this.params, val.params.value);
-              }
-              if(val && val.config && val.config.path){
-                  paths.push(val.config.path);
-              }
-            });
-          }
+        let paths = [];
+        this.params = {};
+        if (allRoutes.length) {
+          allRoutes.map((val) => {
+            if (val && val.params && val.params.value) {
+              this.params = Object.assign(this.params, val.params.value);
+            }
+            if (val && val.config && val.config.path) {
+              paths.push(val.config.path);
+            }
+          });
+        }
 
-          this.path = paths.join("/");
-          this.routeSubject.next(this.route);
-          this.urlSubject.next(this.url);
-          this.configSubject.next(this.config);
-          this.paramsSubject.next(this.params);
-        });
+        this.path = paths.join('/');
+        this.routeSubject.next(this.route);
+        this.urlSubject.next(this.url);
+        this.configSubject.next(this.config);
+        this.paramsSubject.next(this.params);
+      });
   }
 
-  getRoute(){
+  getRoute() {
     return this.routeSubject.asObservable();
   }
 
-  getPath(){
-      return this.pathSubject.asObservable();
+  getPath() {
+    return this.pathSubject.asObservable();
   }
 
-  getUrl(){
+  getUrl() {
     return this.urlSubject.asObservable();
   }
 
-  getConfig(){
+  getConfig() {
     return this.configSubject.asObservable();
   }
 
-  getParams(){
+  getParams() {
     return this.paramsSubject.asObservable();
   }
 
-
-  getRouteValue(){
+  getRouteValue() {
     return this.route || {};
   }
 
-  getPathValue(){
-      return this.path || "";
+  getPathValue() {
+    return this.path || '';
   }
 
-  getUrlValue(){
+  getUrlValue() {
     return this.url;
   }
 
-  getConfigValue(){
+  getConfigValue() {
     return this.config || {};
   }
 
-  getParamsValue(){
+  getParamsValue() {
     return this.params;
   }
 
-   isActivePath(path: string, exact: boolean) {
-        if(this.path){
-            if(exact){
-                if(path == this.path || path == "/"+this.path){
-                    return true;
-                }
-            }else{
-                if(this.path.indexOf(path) === 0 || ("/"+this.path).indexOf(path) === 0){
-                    return true;
-                }
-            }
+  isActivePath(path: string, exact: boolean) {
+    if (this.path) {
+      if (exact) {
+        if (path == this.path || path == '/' + this.path) {
+          return true;
         }
-        return false;
-    };
+      } else {
+        if (
+          this.path.indexOf(path) === 0 ||
+          ('/' + this.path).indexOf(path) === 0
+        ) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
 }
